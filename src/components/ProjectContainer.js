d12321e6b95aa772558e6e6af2f0f810c11a5d39
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-01 15:12:23 -0700

    ProjectContainer add title and links

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -9,14 +9,22 @@ const styles = {
 
 export default function ProjectContainer() {
   return (
-    <div>
-     
-      <div class="card" style={styles.card}>
+    <div class="d-inline-flex p-2">
+      <div class="card " style={styles.card}>
+        <h5 class="card-title mx-auto my-2">Project title</h5>
         <img
           src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640"
           class="card-img-top"
           alt=""
         />
+        <div class="d-inline-flex p-2">
+          <a href="#" class="btn btn-primary mx-auto">
+            Deployed Application
+          </a>
+          <a href="#" class="btn btn-primary mx-auto">
+            GitHub Repository
+          </a>
+        </div>
       </div>
     </div>
   );
