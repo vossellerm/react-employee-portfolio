0d1cbe1ae43f4dd50202a63b8b690e56a30ce249
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-01 23:42:48 -0700

    styling portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,7 +4,7 @@ import ProjectContainer from "../ProjectContainer";
 export default function Portfolio() {
   return (
     <div>
-      <h1>Portfolio</h1>
+      <h1 className="my-4 ms-5">Portfolio</h1>
       <ProjectContainer />
     </div>
   );
