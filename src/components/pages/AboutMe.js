dfc9937cfde1bc5e4df4b9c1d9e5b2b2dcdbe0a0
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-01 21:53:28 -0700

    add profile image

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,25 +1,31 @@
 import React from "react";
+import avatar from "../../images/avatar.png";
+
+const styles = {
+  avatar: {
+    width: "100px",
+  },
+};
 
 export default function AboutMe() {
   return (
     <div>
-  <h1>About Me</h1>
-  <div>
-    <img src="avatar.png" alt />
-  </div>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-    velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-    ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-    non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-    ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-    rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-    tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-    porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-    vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-    fames ac ante ipsum primis in faucibus.
-  </p>
-</div>
-
+      <h1>About Me</h1>
+      <div>
+        <img src={avatar} alt="" style={styles.avatar} />
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
+        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
+        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
+        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
+        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
+        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+        fames ac ante ipsum primis in faucibus.
+      </p>
+    </div>
   );
 }
